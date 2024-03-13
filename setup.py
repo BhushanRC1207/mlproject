c0f1310c11f4d7b36411b8fcea40e3471299a29d
Bhushan VR Chitte <bhushanrchitte2021@gmail.com>
2024-03-13 10:48:54 +0530

    setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(
+    name='mlproject'
+    version='0.0.1',
+    author='bhushan',
+    author_email='bhushanrchitte2021@gmail.com',
+    packages=find_packages(),
+    install_requires=['numpy','pandas','seaboen']
+)
